caf48c6826768401122c3bdc722d7562cac23491
Natanael Felix <f.natanael.felix.b@gmail.com>
2025-02-18 23:39:48 -0300

    First commit

---
+++ src/main/java/com/example/pi_dev_ops_backend/PiDevOpsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pi_dev_ops_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiDevOpsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiDevOpsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pi_dev_ops_backend/PiDevOpsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pi_dev_ops_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiDevOpsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
